af7c71f535e8f74f7b36ae0093024cb0e2ba3ef4
anjeljayswal <anjeljayswal@gmail.com>
2022-11-26 12:15:50 +0545

    jsFun

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const listItems = document.querySelectorAll('nav ul li')
 
 listItems.forEach((item, idx) => {
     item.addEventListener('click',()=>{
+        console.log("click");
         hideallcontents()
         hideallItems()
         item.classList.add('active')
@@ -11,8 +12,44 @@ listItems.forEach((item, idx) => {
 });
 
 function hideallcontents(){
+    
     contents.forEach(content=>content.classList.remove('show'))
 }
 function hideallItems(){
     listItems.forEach(item =>item.classList.remove('active'))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var obj ={
+//     name : "anjali"
+// }
+
+// map?
+// polyfill? 
+//function fn(){
+
+// }
+
+//{1,2,3,4,5}.map((element, index)=> element+2)
+
+// Array.prototype.newMap = function(fn){
+//     console.log(this)
+//     console.log(fn)
+// }
+
+// var arr1 = [1,2,3,4,5,6]
+// arr1.newMap((element, index) = element*2)
